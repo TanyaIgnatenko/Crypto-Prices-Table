d06af85c0edd15dd1da01fa9c8162ee9ec14185e
TanyaIgnatenko <tanigna.work@gmail.com>
2023-09-24 13:57:39 +0700

    Fix

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import './App.css';
 
 function App() {
   useEffect(() => {
-    ReactGA.pageview(window.location.pathname + window.location.search);
+    ReactGA.send("pageview");
   }, []);
 
   return (
